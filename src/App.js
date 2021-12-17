703897328ba8012deb517ae75e49649f27ae26ee
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-12-17 14:36:21 +0100

    :BUILD: => préparation sass + test ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <div>SALUT</div>
   );
 }
 
