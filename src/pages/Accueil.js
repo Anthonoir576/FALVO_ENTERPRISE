b7075350e71c7d0a4182c5230c20e67909eedc91
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-12-20 14:09:37 +0100

    :STYLE: => style Accueil

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import NavBar from '../components/Navigation/NavBar';
 
 const Accueil = () => {
     return (
         <div className='accueil'>
             <NavBar />
+            <div className='accueil-information'>
+                <div>
+                    <img src="./image/photoHEADd.png" alt="le developpeur web" />
+                </div>
+                <div className='information-dev'>
+                    <h1>FALVO Anthony</h1>
+                    <h2>Développeur React / Node</h2>
+                </div>
+                <div className='information-contact'>
+                    <NavLink exact to='/Contact' aria-label='Contact developpeur web'>
+                        Contactez-moi
+                    </NavLink>
+                </div>
+            </div>
         </div>
     );
 };
