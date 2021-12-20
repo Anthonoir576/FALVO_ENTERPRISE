ba6f21fbb65a74836ea949f4fd048f4112a7f243
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-12-20 11:21:32 +0100

    :REFACTOR: => PAssage v6 react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Accueil from "./pages/Accueil";
-
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
 
 function App() {
+
   return (
-    <>
-      <Accueil />
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path='/accueil' element={<Accueil />} />
+          <Route path='*' element={ <Navigate replace to='/accueil' /> }/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <ul className='navigationWeb'>
-            <li>En savoir plus...</li>
+        <ul className='navigation'>
+            <li>
+                <NavLink exact to='/accueil' aria-label='En savoir plus sur le développeur web'>
+                    En savoir plus...
+                </NavLink>
+            </li>
             <li>Compétence</li>
             <li>Projet</li>
             <li>Contact</li>
