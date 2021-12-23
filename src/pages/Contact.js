3b127fc9ffb2a8f077086cba30a35b1b42f0f1a7
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-12-23 09:32:36 +0100

    :FEAT: => partie contact en cours

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,127 @@
-import React from 'react';
+import React from "react";
+import NavBar from "../components/Navigation/NavBar";
 
 const Contact = () => {
-    return (
-        <div>
-            Contact
+  return (
+    <div className="contact">
+      <NavBar />  
+      <div className="contact-container">
+        <div className="contact-legend">
+          <h2>
+            Message&thinsp; <span>/</span>
+            <em>Ecrire un petit mot ?</em>
+          </h2>
+          <h3>
+            hésitez pas à m’envoyer un petit message
+            <br /> de soutien, ou de suggestion. Merci :)
+          </h3>
         </div>
-    );
+
+        <div className="contact-form_contact">
+          <form className="formulaire" action="app.js" method="post">
+            <div className="spaceform">
+              <input
+                type="text"
+                required
+                id="name"
+                name="user_name"
+                minlength="3"
+                maxlength="20"
+                pattern="^[( )a-zA-Z]+$"
+                placeholder="&ensp; Noms *"
+              />
+              <span>Veuillez entrer uniquement des lettres</span>
+            </div>
+            <div className="spaceform">
+              <input
+                type="email"
+                required
+                id="mail"
+                name="user_mail"
+                minlength="10"
+                maxlength="25"
+                placeholder="&ensp; E-mail *"
+              />
+              <span>Veuillez entrer un e-mail valide</span>
+            </div>
+            <div className="spaceform">
+              <input
+                type="text"
+                required
+                id="objet"
+                name="user_objet"
+                minlength="5"
+                maxlength="25"
+                pattern="^[( )a-zA-Z]+$"
+                placeholder="&ensp; Objet *"
+              />
+              <span>Veuillez entrer uniquement des lettres</span>
+            </div>
+            <div className="spaceform">
+              <textarea
+                id="msg"
+                required
+                name="user_message"
+                minlength="10"
+                maxlength="500"
+                placeholder="&ensp; Message *"
+              ></textarea>
+            </div>
+            <div className="buttonsubmit">
+              <button
+                id="valid-formulaire"
+                type="submit"
+                onclick="return confirm ('En cours de MAINTENANCE ! Veuillez utiliser Anthony.falvo.pro@gmail.com pour tous contact, Merci.')"
+              >
+                VALIDER
+              </button>
+            </div>
+          </form>
+
+          <div className="contact-text_map">
+            <div className="contact-text">
+              <div className="icontxt">
+                <img src="image/02.png" alt="telephone" />
+                <p>07.27.76.52.92</p>
+              </div>
+
+              <div className="icontxt">
+                <img src="image/03.png" alt="e-mail" />
+                <p>anthony.falvo.pro@gmail.com</p>
+              </div>
+
+              <div className="icontxt">
+                <img src="image/01.png" alt="adresse" />
+                <p>
+                  269 Rue du capitaine wazny,
+                  <br /> 59182, Montigny-en-Ostrevent, France
+                </p>
+              </div>
+            </div>
+
+            <iframe
+              className="MAP"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20359.153255147678!2d3.165719540537278!3d50.36852805927621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2c6167184d777%3A0x40af13e81645fa0!2sMontigny-en-Ostrevent!5e0!3m2!1sfr!2sfr!4v1623908762042!5m2!1sfr!2sfr"
+              title="map"
+              width="400"
+              height="300"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+            <iframe
+              className="MAP-S"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20364.294020458543!2d3.1767013!3d50.356545149999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1624428366977!5m2!1sfr!2sfr"
+              title="map small"
+              width="300"
+              height="300"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
