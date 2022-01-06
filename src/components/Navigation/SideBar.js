774d6805b57472f7afe51db7561929f723ba7067
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-01-06 11:23:54 +0100

    :FEAT: => mise en forme menu ok

--- src/components/Navigation/SideBar.js
+++ src/components/Navigation/SideBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SideBar = () => {
     return (
         <>
-            <img src="https://raw.githubusercontent.com/Anthonoir576/FALVO_ENTERPRISE/gh-pages/assets/images/menu02.png" alt="Menu latéral" />
+            <img src="https://raw.githubusercontent.com/Anthonoir576/FALVO_ENTERPRISE/gh-pages/assets/images/menuTypeUn.png" alt="Menu latéral" />
         </>
     );
 };
