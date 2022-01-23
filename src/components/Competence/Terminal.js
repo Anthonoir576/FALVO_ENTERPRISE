3fb03c093335f6cd35a452b8557e6679f94c8808
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-01-23 14:39:32 +0100

    :FEAT: => ajout Terminal en cours

--- src/components/Competence/Terminal.js
+++ src/components/Competence/Terminal.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Terminal = () => {
     return (
-        <>
+
+        <div className="container">
             TERMINAL
-        </>
+        </div>
+
     );
 };
 
