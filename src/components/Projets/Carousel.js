a2fb689d966d486a7b7215c3b5cb2bd8ba5840bc
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-10 17:19:57 +0100

    :FEAT: => BUG a revoir demain

--- src/components/Projets/Carousel.js
+++ src/components/Projets/Carousel.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// a verifier car soucie
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+// a verifier car soucie 
 
 const Carousel = () => {
-<Swiper
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
     >
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
@@ -18,6 +30,7 @@ const Carousel = () => {
       <SwiperSlide>Slide 4</SwiperSlide>
       ...
     </Swiper>
+  );
 };
 
 export default Carousel;
