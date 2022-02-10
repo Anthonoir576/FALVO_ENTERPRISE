37070880a93f71aae9a8040b3a0d6d6ee54c9549
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-10 17:17:34 +0100

    :FEAT: => Correction a faire

--- src/components/Projets/Carousel.js
+++ src/components/Projets/Carousel.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+
+// a verifier car soucie
 
 const Carousel = () => {
-    return (
-        <div>
-            En cours
-        </div>
-    );
+<Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
 };
 
 export default Carousel;
