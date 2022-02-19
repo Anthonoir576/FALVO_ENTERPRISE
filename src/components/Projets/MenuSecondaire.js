33cfe548cb9c09520c8d9d02d1073d811fe87f6e
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-19 12:36:57 +0100

    :FEAT: => Début de la logique du menu secondaire sur la partie projet

--- src/components/Projets/MenuSecondaire.js
+++ src/components/Projets/MenuSecondaire.js
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 const MenuSecondaire = () => {
+
+    const [logiciel, setLogiciel]  = useState(false);
+    const [web, setWeb]            = useState(true);
+    const [ue, setUe]              = useState(false);
+
     return (
-        <div>
+        <div className='menuSecondaire'>
             <nav>
-                ici
-                {/* <ul>
-                    <li>Projet Web</li>
-                    <li>Projet Logiciel</li>
-                    <li>Projet Unreal Engine</li>
-                </ul> */}
+                <ul>
+                    <li>
+                        <p 
+                           aria-label='Mes projets de développement web'
+                           className={web === true ? 'active-menuSecondaire' : 'none'}
+                           onClick={() => {
+
+                               if (web === true) {
+                                   setLogiciel(false);
+                                   setUe(false);
+                               }; 
+                           }}
+                        > 
+                            Projet Web    
+                        </p>
+                    </li>
+                    
+                    <li>
+                        <p  
+                           aria-label='Mes projets de développement logiciel'
+                           className={logiciel === true ? 'active-menuSecondaire' : 'none'}
+                           onClick={() => {
+
+                            setLogiciel(!logiciel);
+
+                            if (logiciel === true) {
+                                setWeb(false);
+                                setUe(false);
+                            }; 
+                        }}
+                        >
+                            Projet Logiciel    
+                        </p>
+                    </li>
+                    <li>
+                        <p
+                           aria-label='Mes projets de développement sous unreal engine'
+                           className={ue === true ? 'active-menuSecondaire' : 'none'}
+                           onClick={() => {
+
+                               setUe(!ue);
+
+                               if (ue === true) {
+                                   setWeb(false);
+                                   setLogiciel(false);
+                               }; 
+                           }}
+                        >
+                            Projet Unreal Engine    
+                        </p>
+                    </li>
+                </ul>
             </nav>
         </div>
     );
 };
 
 export default MenuSecondaire;
+
+
+
+
--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -1,13 +1,13 @@
 import React            from 'react';
 import NavBar           from '../components/Navigation/NavBar';
 import MenuSecondaire   from '../components/Projets/MenuSecondaire';
-import Maintenance      from '../components/Maintenance/Maintenance';
+// import Maintenance      from '../components/Maintenance/Maintenance';
 
 const Projets = () => {
     return (
         <div className='myProjets'>
             <NavBar />
-            <Maintenance />
+            {/* <Maintenance /> */}
             <MenuSecondaire />
         </div>
     );
