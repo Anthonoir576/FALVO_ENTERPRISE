4df843d40d423497a5016cfcd7b7cb8019701e40
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-03-12 13:42:00 +0100

    :FEAT: => démarrage de la refonte en ts => dabord les fichiers, et demarrage de la refont de la racine de SRC

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accueil               from "./pages/Accueil.js";
+import APropos               from "./pages/APropos.js";
+import Competence            from "./pages/Competence.js";
+import Projets               from "./pages/Projets.js";
+import Contact               from "./pages/Contact.js";
+import ThemeContextProvider  from "./context/ThemeContext.js";
+
+import { Routes, 
+         Route,
+         BrowserRouter,
+         Navigate }          from "react-router-dom";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeContextProvider>
+        <Routes>
+          <Route path='/accueil' element={ <Accueil /> } />
+          <Route path='/a_propos' element={ <APropos /> } />
+          <Route path='/competence' element={ <Competence /> } />
+          <Route path='/projets' element={ <Projets /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='*' element={ <Navigate replace to='/FALVO_ENTERPRISE/' /> } />
+          <Route path='/FALVO_ENTERPRISE/' element={ <Accueil /> } />
+        </Routes>
+      </ThemeContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React                       from 'react';
+import ReactDOM                    from 'react-dom';
+import App                         from './App';
+import { TerminalContextProvider } from 'react-terminal';
+
+
+import './styles/css/style.css';
+
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <TerminalContextProvider>
+      <App />
+    </TerminalContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
