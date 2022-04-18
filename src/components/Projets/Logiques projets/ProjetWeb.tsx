7af00948431e697b98d3bc4b3f1dd247a5dde4dc
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-04-18 13:46:32 +0200

    :FEAT: => Mise en place dexemple pour le moment de carte

--- src/components/Projets/Logiques projets/ProjetWeb.tsx
+++ src/components/Projets/Logiques projets/ProjetWeb.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+
+
+
+
 const ProjetWeb = () => {
+
+
+
+
   return (
     <div className="project-cards-container">
       <div className="row">
         <div className="myCard2 card">
           <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
             <div className="date">
               <span className="day">18</span>
               <span className="month">Avr</span>
@@ -31,6 +40,7 @@ const ProjetWeb = () => {
         </div>
         <div className="myCard2 card">
           <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
             <div className="date">
               <span className="day">18</span>
               <span className="month">Avr</span>
@@ -56,6 +66,7 @@ const ProjetWeb = () => {
         </div>
         <div className="myCard2 card">
           <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
             <div className="date">
               <span className="day">18</span>
               <span className="month">Avr</span>
@@ -81,6 +92,59 @@ const ProjetWeb = () => {
         </div>
         <div className="myCard2 card">
           <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
+            <div className="date">
+              <span className="day">18</span>
+              <span className="month">Avr</span>
+              <span className="year">2022</span>
+            </div>
+            <div className="data">
+              <div className="content">
+                <span className="author">Auteur</span>
+                <p className="title">
+                  <a href="https://www.google.com/">mon titre</a>
+                </p>
+                <p className="text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ratione perspiciatis sequi quasi veritatis fuga facere,
+                  accusantium odit obcaecati reprehenderit molestiae incidunt
+                  amet asperiores saepe tenetur minus aut aspernatur facilis sit
+                  rem commodi voluptates ipsa illo earum consequatur. Atque, hic
+                  sit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="myCard2 card">
+          <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
+            <div className="date">
+              <span className="day">18</span>
+              <span className="month">Avr</span>
+              <span className="year">2022</span>
+            </div>
+            <div className="data">
+              <div className="content">
+                <span className="author">Auteur</span>
+                <p className="title">
+                  <a href="https://www.google.com/">mon titre</a>
+                </p>
+                <p className="text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ratione perspiciatis sequi quasi veritatis fuga facere,
+                  accusantium odit obcaecati reprehenderit molestiae incidunt
+                  amet asperiores saepe tenetur minus aut aspernatur facilis sit
+                  rem commodi voluptates ipsa illo earum consequatur. Atque, hic
+                  sit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="myCard2 card">
+          <div className="wrapper">
+              <img src="https://via.placeholder.com/150" alt="test" />
             <div className="date">
               <span className="day">18</span>
               <span className="month">Avr</span>
