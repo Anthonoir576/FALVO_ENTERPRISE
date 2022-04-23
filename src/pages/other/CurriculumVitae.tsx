9c286fd82cd31790f231df099b0c81b40e22d119
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-04-23 11:51:04 +0200

    :STYLE: => Préparation de la partie cv version web pour le lien a propos comme prevu lannee derniere

---
+++ src/pages/other/CurriculumVitae.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../components/Navigation/NavBar';
+
+const CurriculumVitae = () => {
+    return (
+        <div className='aPropos curriculumvitae'>
+            <NavBar />
+            prochainement
+        </div>
+    );
+};
+
+export default CurriculumVitae;
