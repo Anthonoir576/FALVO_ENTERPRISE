56f73c6fc14eba62d9da8bec1f5c0416d50f149f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-04-24 12:19:13 +0200

    :FEAT: => Tentative de restructuration de la partie projet sur le menu niveau ux

--- src/components/Icons/IconoirInternet.tsx
+++ src/components/Icons/IconoirInternet.tsx
@@ -2,7 +2,7 @@ import React, { SVGProps } from "react";
 
 export function IconoirInternet(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg width="1em" height="1em" viewBox="0 0 24 24" className="web-icons" {...props}>
       <g fill="none" stroke="currentColor" strokeWidth="1.5">
         <path
           strokeLinecap="round"
