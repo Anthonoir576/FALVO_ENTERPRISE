1a8d30d85f6b45ae4d457b2df280066db044857b
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-01 11:10:49 +0200

    :FEAT: => mappage logique ok

--- src/components/DB/DbProject.tsx
+++ src/components/DB/DbProject.tsx
@@ -4,28 +4,41 @@ export const baseDeDonneeproject :any = {
 
     web: [{
       studentProjects   : [{
+        id        : 1,  
         date      : '25 Sept 2021',
         title     : 'projet 01 openclassrooms',
         Auteur    : [{nom : 'FALVO', prenom : 'Anthony'}],
-        github    : 'http://test.fr/',
+        github    : 'https://github.com/Anthonoir576',
         linkweb   : 'http://test.fr/',
         Resume    : 'mon contenu est ici test',
         content   : ' tous mon contenu la',
         image     : 'ttps://via.placeholder.com/150'
       }, {
+        id        : 2, 
         date      : '10 Oct 2021',
         title     : 'projet 02 openclassrooms',
         Auteur    : [{nom : 'FALVO', prenom : 'Anthony'}],
-        github    : 'http://test.fr/',
+        github    : 'https://github.com/Anthonoir576',
         linkweb   : 'http://test.fr/',
         Resume    : 'mon p2',
         content   : 'contenu explicatif du p2',
         image     : 'ttps://via.placeholder.com/150'
       }, {
+        id        : 3, 
         date      : '12 Oct 2021',
         title     : 'projet 03 openclassrooms',
         Auteur    : [{nom : 'FALVO', prenom : 'Anthony'}],
-        github    : 'http://test.fr/',
+        github    : 'https://github.com/Anthonoir576',
+        linkweb   : 'http://test.fr/',
+        Resume    : 'mon p3',
+        content   : 'contenu explicatif du p3',
+        image     : 'ttps://via.placeholder.com/150'
+      }, {
+        id        : 4, 
+        date      : '12 Oct 2021',
+        title     : 'projet 03 openclassrooms',
+        Auteur    : [{nom : 'FALVO', prenom : 'Anthony'}],
+        github    : 'https://github.com/Anthonoir576',
         linkweb   : 'http://test.fr/',
         Resume    : 'mon p3',
         content   : 'contenu explicatif du p3',
