d5b9b9f24d79aa9e94971679f1a481580f6b679d
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-15 13:31:51 +0200

    :FEAT: => Ajout dun modal de confirmation d'email envoyé toujours sous sweet

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,16 @@
-import React                from "react";
-import { useState }         from "react";
-import { CopyToClipboard }  from 'react-copy-to-clipboard';
-import NavBar               from "../components/Navigation/NavBar";
-import { init }             from "emailjs-com";
+import React                 from "react";
+import { useState }          from "react";
+import { CopyToClipboard }   from 'react-copy-to-clipboard';
+import NavBar                from "../components/Navigation/NavBar";
+import { init }              from "emailjs-com";
 import { alertAdress, 
          alertEmail,
-         alertPhoneNumber } from "../components/Utils/Alerts";
+         alertPhoneNumber }  from "../components/Utils/Alerts";
 import { regexNom,
          regexEmail,
          regexMessage,
-         regexObjet }       from '../components/Utils/Regex';
-
+         regexObjet }        from '../components/Utils/Regex';
+import { alertMailEnvoyer }  from '../components/Utils/Alerts';
 
 declare const window :any;
 
@@ -57,6 +57,8 @@ const Contact = () => {
             
             setMsgSucces('Message envoyé !');
             setSucces(true);
+            alertMailEnvoyer('Message envoyé !');
+
 
             setTimeout(() => {
               setSucces(false);
