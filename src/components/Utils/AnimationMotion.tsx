11faf11dd2696549b7dd0902c09b75fb7fb3d067
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 12:50:45 +0200

    :FEAT: => test framer-motion

--- src/components/Utils/AnimationMotion.tsx
+++ src/components/Utils/AnimationMotion.tsx
@@ -8,9 +8,12 @@
 //~ |                    ACCUEIL                       | 
 //~ ----------------------------------------------------
     export const accueilAnimePictureProfile = [
-        {x: 0,   scale: 1},  //-   Animate
-        {x: 100, scale: 0},  //-   initial
-        {duration : 0.19}    //-   transition
+        {x: 0,   scale: 1},      //- =>   Animate
+        {x: 100, scale: 0},      //- =>   initial
+        {
+            duration : 0.16,
+            ease: "easeInOut"
+        }                       //-  =>  transition
     ];
 //~ ----------------------------------------------------
 
