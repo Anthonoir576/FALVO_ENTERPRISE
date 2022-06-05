070d57d3b41cac9cfd5aa3a1476aa8c1283f8ec6
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 09:17:36 +0200

    :FEAT: => Commentaire partie tsx maintenant, partie index via better comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
+
+// - -------------------------------------------------------
+// - -------------------- IMPORT ---------------------------
+// - ------------------------------------------------------- 
 import React                         from 'react';
 import ReactDOM                      from 'react-dom';
 import App                           from './App';
 import { TerminalContextProvider }   from 'react-terminal';
-
-
+ 
 import './styles/css/style.css';
+// - -------------------------------------------------------
 
 
+
+
+//* --------------------------------------------------------
+//* --- INDEX DE L'APP => rendu injecter dans l'id #root ---
+//* --------------------------------------------------------
 ReactDOM.render(
-
   <React.StrictMode>
     <TerminalContextProvider>
       <App />
     </TerminalContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
-  
 );
+//* --------------------------------------------------------
