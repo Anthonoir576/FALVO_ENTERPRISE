dd20a50fcd04dcc55f93cc57e22057f1579fd55f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 12:29:20 +0200

    :FEAT: => test framer-motion

--- src/components/Utils/AnimationMotion.tsx
+++ src/components/Utils/AnimationMotion.tsx
@@ -8,8 +8,8 @@
 //~ |                    ACCUEIL                       | 
 //~ ----------------------------------------------------
     export const accueilAnimationPictureProfile = [
-        {x: 0, scale: 1},
-        {scale: 0}
+        {x: 0,   scale: 1}, //-   Animate
+        {x: 100, scale: 0}  //-   initial
     ];
 //~ ----------------------------------------------------
 
