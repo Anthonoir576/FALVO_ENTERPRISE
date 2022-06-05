172f8a8bdb67418dab44e9cb50dea42773539863
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 09:40:17 +0200

    :FEAT: => TODO list créer sur lindex pour correction des anomalies classé par majeur, moyen, mineur

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
+/** DOCUMENTATION ET CORRECTION A APPLIQUER *
+ * 
+ * *****************  MAJEUR  ***********************
+ * > ATTENTION ! => les url des images dependent du chemin master, et url github directement. A corriger lors du déployement.
+ * > Les routes sur la page App risque de posé des problèmes par la suite. 
+ * **************************************************
+ *  
+ * ******************  MOYEN  ***********************
+ * - Verifier le .env si toute est correct et mettre à jour .env.example au fur et a mesure.
+ * **************************************************
+ * 
+ * *****************  MINEUR  ***********************
+ * ~ Librairie d'animation a exploité, afin d'améliorer l'expérience utilisateur.
+ * **************************************************
+ */
+
 
 //- ########################################################
 //- --------------------- IMPORT ---------------------------
