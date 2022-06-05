82804c8cc5e164dfd229038c581577de576bbec7
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 13:35:54 +0200

    :FEAT: => test framer-motion

--- src/components/Utils/AnimationMotion.tsx
+++ src/components/Utils/AnimationMotion.tsx
@@ -11,33 +11,74 @@
     //~ ---------------------------------------- 
     //~ ---------- PHOTO DE PROFILE ------------ 
     //~ ---------------------------------------- 
-    export const accueilAnimePictureProfile = [
-        {x: 0,   scale: 1, opacity: 1, rotate: 0},      //-  Animate
-        {x: 100, scale: 0, opacity: 0, rotate: 180},    //-  initial
-        // {                                            //-  transition
-        //     duration : 1,
-        //     type: 'tween',
-        //     ease: "easeOut"
-        // }                                            
+    export const accueilAnimePictureProfile :any = [
+        {                                                //-  Animate
+            x         : 0,
+            y         : [0, 100, 100, 0], 
+            scale     : 1, 
+            opacity   : 1,
+            rotate    : 0
+        },      
+        {                                                //-  initial
+            x         : 100,
+            y         : 0,
+            scale     : 0,
+            opacity   : 0,
+            rotate    : 180
+        },    
+        {                                               //-  transition
+            duration  : 1,
+            repeat    : Infinity
+        }                                            
     ];
     
     //~ ---------------------------------------- 
     //~ ----- txt effet + h2 + btn contact ----- 
     //~ ---------------------------------------- 
-    export const accueilAnimeTxt01 = [
-        {x: 0,   scale: 1, opacity: 1},                 //-  Animate
-        {x: -150, scale: 0.5, opacity: 0},              //-  initial
-        {duration: 0.35}                                //-  transition
+    export const accueilAnimeTxt01 :any = [
+        {                                               //-  Animate
+            x        : 0, 
+            scale    : 1,
+            opacity  : 1
+        },                 
+        {
+            x        : -150,
+            scale    : 0.5,
+            opacity  : 0
+        },                                              //-  initial
+        {
+            duration : 0.35
+        }                                               //-  transition
     ];
-    export const accueilAnimeTxt02 = [
-        {x: 0,   scale: 1, opacity: 1},                 //-  Animate
-        {x: -150, scale: 0.5, opacity: 0},              //-  initial
-        {duration: 0.45}                                //-  transition
+    export const accueilAnimeTxt02 :any = [
+        {                                               //-  Animate
+            x        : 0, 
+            scale    : 1,
+            opacity  : 1
+        },                 
+        {
+            x        : -150,
+            scale    : 0.5,
+            opacity  : 0
+        },                                              //-  initial
+        {
+            duration : 0.45
+        }                                               //-  transition
     ];
-    export const accueilAnimeTxt03 = [
-        {x: 0,   scale: 1, opacity: 1},                 //-  Animate
-        {x: -150, scale: 0.5, opacity: 0},              //-  initial
-        {duration: 0.55}                                //-  transition
+    export const accueilAnimeTxt03 :any = [
+        {                                               //-  Animate
+            x        : 0, 
+            scale    : 1,
+            opacity  : 1
+        },                 
+        {
+            x        : -150,
+            scale    : 0.5,
+            opacity  : 0
+        },                                              //-  initial
+        {
+            duration : 0.55
+        }                                               //-  transition
     ];
 //~ ----------------------------------------------------
 
