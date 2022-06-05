45a723769386d0ba158a3324ab64bc39359570ad
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-05 15:41:08 +0200

    :FEAT: => Amélioration intéressante en cours sur la partie modaux

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -37,52 +37,57 @@ const Accueil = () => {
 
 
     return (
-        <motion.div className='accueil'
-            // animate={{y : 0}}
-            // initial={{y: -2500}}
-            // transition={{duration :1.5}}
-        >
-            <NavBar />
-            <div className='accueil-information'>
-                <motion.div className='information-image' 
-                            animate={ accueilAnimePictureProfile[0] }
-                            initial={ accueilAnimePictureProfile[1] }
-                            // transition={accueilAnimePictureProfile[2]}
-                >
-                    <img src={"https://raw.githubusercontent.com/Anthonoir576/FALVO_ENTERPRISE/gh-pages/assets/images/photo-profil.png"} alt="le developpeur web" />
-                </motion.div>
-                <div className="text-container">
-                    <div className='information-dev'>
-                        <motion.h1 className='text-effect'
-                                   animate={    accueilAnimeTxt01[0]  }
-                                   initial={    accueilAnimeTxt01[1]  }
-                                   transition={ accueilAnimeTxt01[2] }
-                        >
-                            FALVO Anthony
-                        </motion.h1>
-                        <motion.h2 animate={    accueilAnimeTxt02[0] }
-                                   initial={    accueilAnimeTxt02[1] }
-                                   transition={ accueilAnimeTxt02[2] }
-                        
-                        >
-                            Développeur React <span>&amp;</span> Node
-                        </motion.h2>
-                    </div>
-                    <motion.div className='information-contact'
-                                animate={    accueilAnimeTxt03[0] }
-                                initial={    accueilAnimeTxt03[1] }
-                                transition={ accueilAnimeTxt03[2] }
-                    >
-                        <NavLink to='/contact' aria-label='Contact developpeur web'>
-                            Contactez-moi
-                        </NavLink>
-                    </motion.div>
-                </div>
+      <motion.div
+        className="accueil"
+      >
+        <NavBar />
+        <div className="accueil-information">
+          <motion.div
+            className="information-image"
+            animate={accueilAnimePictureProfile[0]}
+            initial={accueilAnimePictureProfile[1]} 
+          >
+            <img
+              src={
+                "https://raw.githubusercontent.com/Anthonoir576/FALVO_ENTERPRISE/gh-pages/assets/images/photo-profil.png"
+              }
+              alt="le developpeur web"
+            />
+          </motion.div>
+          <div className="text-container">
+            <div className="information-dev">
+              <motion.h1
+                className="text-effect"
+                animate={accueilAnimeTxt01[0]}
+                initial={accueilAnimeTxt01[1]}
+                transition={accueilAnimeTxt01[2]}
+              >
+                FALVO Anthony
+              </motion.h1>
+              <motion.h2
+                animate={accueilAnimeTxt02[0]}
+                initial={accueilAnimeTxt02[1]}
+                transition={accueilAnimeTxt02[2]}
+              >
+                Développeur React <span>&amp;</span> Node
+              </motion.h2>
             </div>
-            <div className="accueil-switch">
-                <p>^</p>
-            </div>
-        </motion.div>
+            <motion.div
+              className="information-contact"
+              animate={accueilAnimeTxt03[0]}
+              initial={accueilAnimeTxt03[1]}
+              transition={accueilAnimeTxt03[2]}
+            >
+              <NavLink to="/contact" aria-label="Contact developpeur web">
+                Contactez-moi
+              </NavLink>
+            </motion.div>
+          </div>
+        </div>
+        <div className="accueil-switch">
+          <p>^</p>
+        </div>
+      </motion.div>
     );
 };
 //* --------------------------------------------------------
