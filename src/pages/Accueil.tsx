34db60dda9cd9c12e545eae904f6d2b9cbd6dc00
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-06 09:37:22 +0200

    :FEAT: => En cours de test dune librairie tsparticle

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -8,22 +8,22 @@ import NavBar                             from '../components/Navigation/NavBar'
 
 //      <--------------- ANIMATION -------------------->
 import { txtEffect }                      from '../components/Utils/EffectTxt';
-import { motion }                         from 'framer-motion';
-import { AnimatePresence }                from 'framer-motion';  
+// import { motion }                         from 'framer-motion';
+// import { AnimatePresence }                from 'framer-motion';  
 
-// photo profile
-import { accueilAnimePictureProfile}      from '../components/Utils/AnimationMotion';
+// // photo profile
+// import { accueilAnimePictureProfile}      from '../components/Utils/AnimationMotion';
 
-// text
-import { accueilAnimeTxt01 }              from '../components/Utils/AnimationMotion';
-import { accueilAnimeTxt02 }              from '../components/Utils/AnimationMotion';
-import { accueilAnimeTxt03 }              from '../components/Utils/AnimationMotion';
+// // text
+// import { accueilAnimeTxt01 }              from '../components/Utils/AnimationMotion';
+// import { accueilAnimeTxt02 }              from '../components/Utils/AnimationMotion';
+// import { accueilAnimeTxt03 }              from '../components/Utils/AnimationMotion';
 
-// navigation accueil
-import { accueilAnimeNavigation }         from "../components/Utils/AnimationMotion";
+// // navigation accueil
+// import { accueilAnimeNavigation }         from "../components/Utils/AnimationMotion";
 
-// transition pages
-import { animeTransitionPages }           from '../components/Utils/AnimationMotion';
+// // transition pages
+// import { animeTransitionPages }           from '../components/Utils/AnimationMotion';
 //      <---------------------------------------------->
 //- --------------------------------------------------------
 //- ########################################################
