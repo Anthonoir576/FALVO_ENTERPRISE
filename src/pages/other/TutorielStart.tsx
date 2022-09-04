157b0454d4a9fbb23abab9835cb2e33c6623ec81
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-09-04 10:48:12 +0200

    :FEAT: => Partie tuto - Partie tuto start - En cours de test ui

--- src/pages/other/TutorielStart.tsx
+++ src/pages/other/TutorielStart.tsx
@@ -20,15 +20,71 @@ const TutorielStart = () => {
       <NavBar />
       <Back />
       <div className="tutoriel-start">
-        <div className='progress-bar'>
-          <span className='test'></span>
+        <div className="progress-bar">
+          <span className="test"></span>
         </div>
-        <div className='container-main'>
-          <div className='cours'>
-            COURS
+        <div className="container-main">
+          <div className="cours">
+            <h2>titre cours</h2>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              aspernatur possimus excepturi cumque soluta, perspiciatis totam
+              pariatur hic, neque assumenda, corrupti numquam ullam deserunt
+              laudantium tempora qui molestiae placeat! Iusto impedit culpa
+              dolorum aut amet eum ut quam quaerat nulla laudantium inventore
+              dicta ipsam, odio voluptas quod officiis cupiditate eius tempora
+              asperiores temporibus consequuntur quas! Impedit cumque est
+              maiores iusto cupiditate reprehenderit voluptatum architecto odit
+              alias, maxime minima placeat odio modi optio ipsa dignissimos
+              quidem dolor. Quam nobis culpa autem reiciendis voluptas unde
+              earum aut ratione pariatur ducimus cupiditate enim soluta
+              provident fugiat perspiciatis, animi consequuntur porro? Ipsam,
+              doloremque dignissimos.
+            </p>
+
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              aspernatur possimus excepturi cumque soluta, perspiciatis totam
+              pariatur hic, neque assumenda, corrupti numquam ullam deserunt
+              laudantium tempora qui molestiae placeat! Iusto impedit culpa
+              dolorum aut amet eum ut quam quaerat nulla laudantium inventore
+              dicta ipsam, odio voluptas quod officiis cupiditate eius tempora
+              asperiores temporibus consequuntur quas! Impedit cumque est
+              maiores iusto cupiditate reprehenderit voluptatum architecto odit
+              alias, maxime minima placeat odio modi optio ipsa dignissimos
+              quidem dolor. Quam nobis culpa autem reiciendis voluptas unde
+              earum aut ratione pariatur ducimus cupiditate enim soluta
+              provident fugiat perspiciatis, animi consequuntur porro? Ipsam,
+              doloremque dignissimos.
+            </p>
+
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              aspernatur possimus excepturi cumque soluta, perspiciatis totam
+              pariatur hic, neque assumenda, corrupti numquam ullam deserunt
+              laudantium tempora qui molestiae placeat! Iusto impedit culpa
+              dolorum aut amet eum ut quam quaerat nulla laudantium inventore
+              dicta ipsam, odio voluptas quod officiis cupiditate eius tempora
+              asperiores temporibus consequuntur quas! Impedit cumque est
+              maiores iusto cupiditate reprehenderit voluptatum architecto odit
+              alias, maxime minima placeat odio modi optio ipsa dignissimos
+              quidem dolor. Quam nobis culpa autem reiciendis voluptas unde
+              earum aut ratione pariatur ducimus cupiditate enim soluta
+              provident fugiat perspiciatis, animi consequuntur porro? Ipsam,
+              doloremque dignissimos.
+            </p>
           </div>
-          <div className='chapitre'>
-            CHAPITRE
+          <div className="chapitre">
+            <p>CHAPITRE :</p>
+            <p>chapitre 1</p>
+            <p>chapitre 2</p>
+            <p>chapitre 3</p>
+            <p>chapitre 4</p>
+            <p>chapitre 5</p>
+            <p>chapitre 6</p>
           </div>
         </div>
       </div>
